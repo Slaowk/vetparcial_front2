c12acd925afb8ea0150a7bb8c032862338a9aa81
slaowk <proerickbarr@gmail.com>
2023-09-06 23:22:27 -0500

    fisrt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MascotaComponent } from './components/mascota/mascota.component';
+import { AlimentoComponent } from './components/alimento/alimento.component';
+import { DuenoComponent } from './components/dueno/dueno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Mascotas", component:MascotaComponent},
+  {path:"Alimentos", component:AlimentoComponent},
+  {path:"Duenos", component:DuenoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
